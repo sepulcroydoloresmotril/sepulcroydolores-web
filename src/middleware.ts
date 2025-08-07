a5d2102febda5dd79ad428495c4b9b82a879859a
Fran Enríquez <fran.enriquez.dev@gmail.com>
2025-08-07 12:21:50 +0200

    FIX: restaurar redirección de bono-feria en la configuración y descomentar middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  if (context.url.pathname === "https://sepulcroydoloresmotril.es/bono-feria") {
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: "https://buy.stripe.com/eVq3cw6GS9q39KHas47ss05",
-      },
-    });
-  }
+//   if (context.url.pathname === "https://sepulcroydoloresmotril.es/bono-feria") {
+//     return new Response(null, {
+//       status: 302,
+//       headers: {
+//         Location: "https://buy.stripe.com/eVq3cw6GS9q39KHas47ss05",
+//       },
+//     });
+//   }
 
   return next();
 });
