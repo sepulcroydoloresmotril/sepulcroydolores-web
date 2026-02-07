19a067454d2c84297265ef707f75512584a89ea0
Fran Enríquez <fran.enriquez.dev@gmail.com>
2026-02-07 12:14:44 +0100

    feat: agregar redirección para formulario utilizando URL de Google Forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,20 @@ import preact from "@astrojs/preact";
 
 import vercel from "@astrojs/vercel/serverless";
 
+const googleForm = process.env.PUBLIC_GOOGLE_FORM_URL;
+
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/': {
       status: 302,
       destination: '/links'
-    }
+    },
+    '/formulario': {
+      destination: googleForm || 'https://forms.gle/2Zt3n9mLh7sHj5cM8',
+      status: 302,
+    },
   },
   integrations: [tailwind(), preact()],
   output: "server",
